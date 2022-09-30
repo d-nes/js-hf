33f4fefab12e0f655f5ff40fbdd89c2f7c411161
Dénes Urbán <urband@edu.bme.hu>
2022-09-30 14:29:31 +0200

    functionally done

--- index.js
+++ index.js
@@ -3,10 +3,28 @@ const path = require('path');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
+    res.sendFile(path.join(__dirname, 'source/index.html'));
 })
 
-//app.use(express.static('static'));
+app.get('/details', (req, res) => {
+    res.sendFile(path.join(__dirname, 'source/show_details.html'));
+})
+
+app.get('/new_show', (req, res) => {
+    res.sendFile(path.join(__dirname, 'source/new_show.html'));
+})
+
+app.get('/edit_show', (req, res) => {
+    res.sendFile(path.join(__dirname, 'source/edit_show.html'));
+})
+
+app.get('/new_episode', (req, res) => {
+    res.sendFile(path.join(__dirname, 'source/new_episode.html'));
+})
+
+app.get('/edit_episode', (req, res) => {
+    res.sendFile(path.join(__dirname, 'source/edit_episode.html'));
+})
 
 app.listen(3000, () => {
     console.log('Running on :3000');
