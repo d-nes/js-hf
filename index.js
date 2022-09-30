3f04b6fcad95be104dca6a049dbf680c3b5eb696
Dénes Urbán <urband@edu.bme.hu>
2022-09-30 12:13:08 +0200

    IT WORKS

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.sendFile(path.join(__dirname, '/index.html'));
 })
 
-app.use(express.static('static'));
+//app.use(express.static('static'));
 
 app.listen(3000, () => {
     console.log('Running on :3000');
