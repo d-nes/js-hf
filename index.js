adc7103210d675e920d51e6ea510eb85f8361058
Dénes Urbán <urband@edu.bme.hu>
2022-10-14 19:35:15 +0200

    extracted basic routing

--- index.js
+++ index.js
@@ -1,30 +1,7 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'source/index.html'));
-})
-
-app.get('/details', (req, res) => {
-    res.sendFile(path.join(__dirname, 'source/show_details.html'));
-})
-
-app.get('/new_show', (req, res) => {
-    res.sendFile(path.join(__dirname, 'source/new_show.html'));
-})
-
-app.get('/edit_show', (req, res) => {
-    res.sendFile(path.join(__dirname, 'source/edit_show.html'));
-})
-
-app.get('/new_episode', (req, res) => {
-    res.sendFile(path.join(__dirname, 'source/new_episode.html'));
-})
-
-app.get('/edit_episode', (req, res) => {
-    res.sendFile(path.join(__dirname, 'source/edit_episode.html'));
-})
+require('./routing/index')(app);
 
 app.listen(3000, () => {
     console.log('Running on :3000');
---
+++ routing/index.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = function (app) {
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, '../source/index.html'));
+    })
+
+    app.get('/details', (req, res) => {
+        res.sendFile(path.join(__dirname, '../source/show_details.html'));
+    })
+
+    app.get('/new_show', (req, res) => {
+        res.sendFile(path.join(__dirname, '../source/new_show.html'));
+    })
+
+    app.get('/edit_show', (req, res) => {
+        res.sendFile(path.join(__dirname, '../source/edit_show.html'));
+    })
+
+    app.get('/new_episode', (req, res) => {
+        res.sendFile(path.join(__dirname, '../source/new_episode.html'));
+    })
+
+    app.get('/edit_episode', (req, res) => {
+        res.sendFile(path.join(__dirname, '../source/edit_episode.html'));
+    })
+}
