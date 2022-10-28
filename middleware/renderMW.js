2371bcb9ead6011d946403ba4f1710dc84f83c4a
Dénes Urbán <urband@edu.bme.hu>
2022-10-28 10:53:52 +0200

    figured out templating yey

--- middleware/renderMW.js
+++ middleware/renderMW.js
@@ -8,6 +8,7 @@
 module.exports = function (viewName, path, objects) {
     return function (req, res) {
         console.log(`Render: ${viewName} ${req.params.showid} ${req.params.episodeid}`);
-        res.sendFile(path.join(__dirname, `../source/${viewName}.html`));
+        //res.sendFile(path.join(__dirname, `../source/${viewName}.html`));
+        res.render(path.join(__dirname, `../views/pages/${viewName}.ejs`))
     }
 }
