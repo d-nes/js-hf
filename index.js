c81c58778718cc5d78582a6ef825b8c178340851
Dénes Urbán <urband@edu.bme.hu>
2022-10-28 10:58:47 +0200

    dont know what this does

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const ejs = require('ejs');
 
 require('./routing/index')(app, path);
+
+app.set('view engine', 'ejs');
 
 app.listen(3000, () => {
     console.log('Running on :3000');
